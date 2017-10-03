f49135fc965e1be711995cc7c1ee4a9736363502
rajul.shukla <rajul.shukla@incedoinc.com>
2017-10-03 19:38:27 +0530

    Adding user service registry

---
+++ user-service/src/main/java/com/threaddynamics/user/service/config/Config.java
@@ -0,0 +1,15 @@
+package com.threaddynamics.user.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Config {
+	
+	@Bean
+	public RestTemplate producesRestTemplate(){
+		return new RestTemplate();
+	}
+
+}
--- user-service/src/main/java/com/threaddynamics/user/service/main/UserServiceApplication.java
+++ user-service/src/main/java/com/threaddynamics/user/service/main/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.threaddynamics.user.service.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(scanBasePackages = "com.threaddynamics.*")
+@EnableEurekaClient
 public class UserServiceApplication {
 	
 	public static void main(String args[])
